c42fe85c4ccd76cd3cd9f56020e9ba22bbd77387
Philip Cho <chohyu01@cs.washington.edu>
2017-09-28 10:51:31 -0700

    Redirect build_wheel to build_bdist_wheel
    Use patch to change default multibuild implementation

--- config.sh
+++ config.sh
@@ -49,5 +49,5 @@ function pre_build {
 function run_tests {
   # Runs tests on installed distribution from an empty directory
   python --version
-  python -c 'import sys; import treelite; sys.exit(yourpackage.test())'
+  python -c 'import sys; import treelite'
 }
